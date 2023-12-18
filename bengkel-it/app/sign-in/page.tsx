8212f2c02cea213c447df5c3a0c92ff5e0f0f196
Rezrfm <reza.maulana@students.paramadina.ac.id>
2023-12-18 22:45:36 +0700

    chore: route on sign-in and sign-up

--- bengkel-it/app/sign-in/page.tsx
+++ bengkel-it/app/sign-in/page.tsx
@@ -6,58 +6,35 @@ import IconUsername from "@/public/svgs/icon-username";
 import Label from "@/components/Label/Index";
 
 export default function SignIn() {
-  return (
-    <div className="pl-[100px] mx-auto flex flex-row h-screen">
-      <div className="flex flex-col w-[40%] pr-[125px]">
-        <Logo className="mt-[28px]" />
-        <div className="flex flex-col mt-[100px]">
-          <h1 className="font-poppins text-black text-[44px] font-bold leading-[130%]">
-            We are <br />
-            the <span className="text-blue-main">navigation</span>.
-          </h1>
-          <p className="text-[#6B7588] text-[16px] font-poppins font-bold leading-[130%] mt-[16px]">
-            Welcome back, <br />
-            please login to your account.
-          </p>
-        </div>
-        <div className="flex flex-col mt-[25px] w-[51vh]">
-          <Label
-            name="Username or email"
-            type="email"
-            icon={<IconUsername />}
-          />
-          <Label
-            name="Password"
-            forgot="Forgot Password?"
-            type="password"
-            icon={<IconPassword />}
-            className="mt-[16px]"
-          />
-          <button className="w-full bg-blue-main py-[17px] rounded-[10px] text-[#ffff] text-[16px] font-poppins font-normal leading-[170%] mt-[24px]">
-            Sign in
-          </button>
-        </div>
-        <div className="flex flex-row gap-[3px] mt-[8px]">
-          <p className="text-black text-[16px] font-normal leading-[170%]">
-            Not have an account?
-          </p>
-          <Link
-            href={"#"}
-            className="text-blue-main text-[16px] font-normal leading-[170%]"
-          >
-            Sign Up
-          </Link>
-        </div>
-      </div>
-      <div className="w-[60%]">
-        <Image
-          src={"/images/login.png"}
-          width={1000}
-          height={1024}
-          alt="hero"
-          className="w-full"
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="pl-[100px] mx-auto flex flex-row h-screen">
+			<div className="flex flex-col w-[40%] pr-[125px]">
+				<Logo className="mt-[28px]" />
+				<div className="flex flex-col mt-[100px]">
+					<h1 className="font-poppins text-black text-[44px] font-bold leading-[130%]">
+						We are <br />
+						the <span className="text-blue-main">navigation</span>.
+					</h1>
+					<p className="text-[#6B7588] text-[16px] font-poppins font-bold leading-[130%] mt-[16px]">
+						Welcome back, <br />
+						please login to your account.
+					</p>
+				</div>
+				<div className="flex flex-col mt-[25px] w-[51vh]">
+					<Label name="Username or email" type="email" icon={<IconUsername />} />
+					<Label name="Password" forgot="Forgot Password?" type="password" icon={<IconPassword />} className="mt-[16px]" />
+					<button className="w-full bg-blue-main py-[17px] rounded-[10px] text-[#ffff] text-[16px] font-poppins font-normal leading-[170%] mt-[24px]">Sign in</button>
+				</div>
+				<div className="flex flex-row gap-[3px] mt-[8px]">
+					<p className="text-black text-[16px] font-normal leading-[170%]">Not have an account?</p>
+					<Link href={"/sign-up"} className="text-blue-main text-[16px] font-normal leading-[170%]">
+						Sign Up
+					</Link>
+				</div>
+			</div>
+			<div className="w-[60%]">
+				<Image src={"/images/login.png"} width={1000} height={1024} alt="hero" className="w-full" />
+			</div>
+		</div>
+	);
 }
